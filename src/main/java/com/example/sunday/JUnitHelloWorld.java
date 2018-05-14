58d4d15f3099f06b49b6ca8c01d41c887530e925
Santha Perian <snowmaniowa@users.noreply.github.com>
2018-05-14 18:16:05 -0500

    rename params

--- src/main/java/com/example/sunday/JUnitHelloWorld.java
+++ src/main/java/com/example/sunday/JUnitHelloWorld.java
@@ -2,7 +2,7 @@ package com.example.sunday;
 
 public class JUnitHelloWorld {
 
-    public boolean isGreater(int num11, int num12) {
-        return num11 > num12;
+    public boolean isGreater(int num1, int num2) {
+        return num1 > num2;
     }
 }
